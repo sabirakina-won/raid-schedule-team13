5c63d4572bef2fb4e29586c6e0501d0235ebc4d1
sabirakina-won <sabirakina@gmail.com>
2026-08-01 15:22:20 +0500

    АВА

--- main.go
+++ main.go
@@ -1,49 +1,49 @@
-package main 
+package main
 
 import "fmt"
 
-func printMessage(rows []string, busy string, important string) {
+func printSchedule(rows []string, busy string, important string) {
 	errorMessage := validate(rows)
 
 	if errorMessage != "" {
-		fmtPrintln(errorMessage)
+		fmt.Println(errorMessage)
 		return
 	}
 
-columns := len(rows[0])
+	columns := len(rows[0])
 
-for _, row := range rows {
-	printBorder (columns)
-	fmt.Print("|")
+	for _, row := range rows {
+		printBorder(columns)
+		fmt.Print("|")
 
-	for i := 0; i < columns; i++ {
-		cell := string(row[i])
+		for i := 0; i < columns; i++ {
+			cell := string(row[i])
 
-		if cell == "0" {
-			fmr.Print(makeCell("", ""))
-		} else if cell == "1" {
-			fmt.Print(maleCell(busy, "red"))
-		} else {
-			fmt.Print(makeCell(important, "yellow"))
+			if cell == "0" {
+				fmt.Print(makeCell("", ""))
+			} else if cell == "1" {
+				fmt.Print(makeCell(busy, "red"))
+			} else {
+				fmt.Print(makeCell(important, "yellow"))
+			}
+
+			fmt.Print("|")
 		}
 
-		fmt.Print("|")
-	} 
+		fmt.Println()
+	}
 
-	fmt.Println()
-}
-
-printBorder(columns)
+	printBorder(columns)
 }
 
 func main() {
-	printSchedule([]string{"0102", "01100", "2001"}, "###", "!!!")
+	printSchedule([]string{"0102", "0110", "2001"}, "###", "!!!")
 
 	fmt.Println()
-    
-	printSchedule([]string{"01020", "011002", "00210"}, "*", "$")
 
-	fmt.println()
+	printSchedule([]string{"01020", "01102", "00210"}, "*", "$")
+
+	fmt.Println()
 
 	printSchedule([]string{"0102", "1X00"}, "###", "!!!")
 }
